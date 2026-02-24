39f10ed34b90d41b8de472aedc1693a7d9c8acce
Mayorrkun <mayokunlaoshelawal@gmail.com>
2026-02-24 18:25:27 +0100

    Implemented Conditional Rendering

--- chef-claude/src/components/Main.jsx
+++ chef-claude/src/components/Main.jsx
@@ -1,15 +1,62 @@
 import "../css/Main.css";
+import { useState } from "react";
+
+
 
 function Main(){
+//"Chicken", "Oregano", "Tomatoes"
+const [ingredients, setIngredients] = useState(["Chicken breasts","Most of the main spices","Olive oil", "Heavy Cream", "Chicken broth", "Parmesan cheese", "Spinach"]);
 
-    const Main =
-    <main> <form className="main-form" action="/">
+  function onSubmit(formData){
+     const newIngredient = formData.get("ingredient");
+     if(typeof newIngredient === 'string' && newIngredient.trim().length !== 0){
+        setIngredients(ingredients => [...ingredients, newIngredient]);
+     }
+}
+
+function recipeReady(){
+      return <div className="recipe-ready">
+          <div>
+              <span>Ready for a recipe?</span>
+              <p>Generate a recipe from a list of ingredients</p>
+          </div>
+          <button>
+              Get a recipe
+          </button>
+      </div>
+}
+
+function onHand(){
+      const body =  <div className="ingredients-list">
+          <span>Ingredients on hand:</span>
+          <ul>
+              {ingredients.map((ingredient, index) => (
+                 <li key={index}>{ingredient}</li>
+              ))}
+          </ul>
+
+          {ingredients.length > 3 ? recipeReady() : null}
+      </div>
+
+    return ingredients.length === 0 ? null : body;
+
+
+}
+    
+
+    const Main = <main> <form className="main-form" action={onSubmit}>
         
-        <input type="text" placeholder="e.g. oregano"/>
-        <button type="button">+ Add ingredient</button>
+        <input aria-label="Add ingredient" type="text" name="ingredient" placeholder="e.g. oregano"/>
+        <button>+ Add ingredient</button>
 
         </form>
+
+        {onHand()}
+
+     
         </main>
+
+        
     
 
     return Main
